5a991c5b78e35781420374f6d6552adb68d4f613
Daelo Namaumbo <bsc-53-16@cc.ac.mw>
2022-10-29 16:00:56 +0200

    ok

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Home () {
 return(
     <>
-    <h2 style={{color:"black",fontWeight:"150",fontSize:"35px"}}>Welcome Back Daelo,</h2>
+    <h2 style={{color:"black",fontWeight:"150",fontSize:"35px"}}>Welcome Back,</h2>
     <span style={{color:"black",fontSize:"15px"}}>You have 3 task to complete to day</span>
     </>
 )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -11,56 +11,68 @@ import {
 } from "semantic-ui-react";
 import "./Tasks.css";
 export default function Tasks() {
-  const options = [
-    { key: "high", text: "high", value: "3" },
-    { key: "delete", text: "medium", value: "2" },
-    { key: "hide", text: "low", value: "1" },
-  ];
-
   const [check, setChecked] = useState(false);
-  const [complete, setCompleted] = useState(" mark complete");
+  const [complete, setCompleted] = useState("mark complete");
   const [btnColor, setBtnColor] = useState("orange");
   const [iconName, setIconName] = useState("");
   const [open, setOpen] = useState(false);
   const [heading, setHeading] = useState("");
   const [description, setDescription] = useState("");
-
-  const [details, setDetails] = useState({ head: "", descrpt: "" });
+  // const [details, setDetails] = useState({ head});
   const [todoList, setTodoList] = useState([]);
+  const [remove, setRemove] = useState(false);
 
-  const handleChange = (e) => {
-    setHeading(e.target.value);
-  };
-
-  const handleDescription = (e) => {
-    setDescription(e.target.value);
-  };
-  setTimeout(() => {
-    setDetails({
-      head: heading,
-      descrpt: description,
+  function deleteItem(key) {
+    var filteredItems = todoList.filter(function (item) {
+      return item.key !== key;
     });
-   
-  }, 1000);
+
+    setTodoList(filteredItems);
+  }
+
+  // setTimeout(() => {
+  //   setDetails({
+  //     head: heading,
+  //     descrpt: description,
+  //   });
+  // }, 1000);
+
   const doneInp = () => {
     setOpen(false);
-    setTodoList((prev) => [...prev, details]);
+    // setTodoList((prev) => [...prev, details]);
   };
+
   const addTask = () => {
     setOpen(true);
   };
-  const markdone = () => {
-    setChecked(true);
-    setCompleted("completed");
-    setBtnColor("green");
-    setIconName("check");
 
-    setTimeout(() => {
-      setBtnColor("red");
-      setIconName("cancel");
-      setCompleted("delete");
-    }, 5000);
-  };
+  function addItem(e) {
+    let newItem = {
+      text: heading,
+      key: Date.now(),
+    };
+    setTodoList((prevState) => [...prevState, newItem]);
+    setHeading("");
+    setOpen(false);
+
+    e.preventDefault();
+  }
+  // const markdone = () => {
+  //   setChecked(true);
+  //   setCompleted("completed");
+  //   setBtnColor("green");
+  //   setIconName("check");
+
+  //   setTimeout(() => {
+  //     setBtnColor("red");
+  //     setIconName("cancel");
+  //     setCompleted("delete");
+  //   }, 5000);
+
+  //   if (complete.match("delete")) {
+  //   }
+  // };
+  console.log(todoList);
   return (
     <>
       {" "}
@@ -75,23 +87,32 @@ export default function Tasks() {
           <>
             {" "}
             <Segment>
-              <b style={{ color: "black" }}>Subject</b>
-              <Input onChange={handleChange} fluid />
+              <form onSubmit={addItem}>
+                <b style={{ color: "black" }}>Subject</b>
+                <br />
+                <input
+                  onChange={(e) => setHeading(e.target.value)}
+                  placeholder="enter task"
+                ></input>
+                <br />
+                <Button type="submit" color="green" compact size="tiny">
+                  add
+                </Button>
+              </form>
+              {/* 
               <br />
               <b style={{ color: "black" }}>Description</b>
               <Input onChange={handleDescription} fluid />
-              <br />
-              <Button color="green" compact size="tiny" onClick={doneInp}>
-                Done
-              </Button>
-              <Button.Group color="teal" compact size="tiny">
+              <br /> */}
+
+              {/* <Button.Group color="teal" compact size="tiny">
                 <Button>priority</Button>
                 <Dropdown
                   className="button icon"
                   options={options}
                   trigger={<></>}
                 />
-              </Button.Group>
+              </Button.Group> */}
             </Segment>
           </>
         ) : (
@@ -101,45 +122,52 @@ export default function Tasks() {
       <br />
       <div className="tasks">
         
-        {todoList.map((item) => {
-          return (
-            <div key={item.head} >
-              <Segment color="red" >
-                <List>
-                  <List.Item>
-                    <List.Icon name="marker" />
-                    <List.Content >
-                      <List.Header>
-                        <Checkbox checked={check} label={item.head} />{" "}
-                      </List.Header>
-                      <List.Description>{item.description}</List.Description>
-                      <span
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                          marginTop: "5px",
-                        }}
-                      >
-                        <Label>
-                          <Icon name="cart arrow down"></Icon>marketing
-                        </Label>
-                        <Label
-                          color={btnColor}
-                          as="a"
-                          size="tiny"
-                          onClick={markdone}
+          {" "}
+          {todoList.map((item) => {
+            return remove ? (
+              <></>
+            ) : (
+              
+                <Segment color="red"key={item.key}  >
+                  <List>
+                    <List.Item>
+                      <List.Icon name="marker" />
+                      <List.Content>
+                        <List.Header>
+                          <Checkbox checked={check} label={item.text} />{" "}
+                        </List.Header>
+                        <List.Description>{item.description}</List.Description>
+                        <span
+                          style={{
+                            display: "flex",
+                            justifyContent: "space-between",
+                            marginTop: "5px",
+                          }}
                         >
-                          <Icon name={iconName} />
-                          {complete}
-                        </Label>
-                      </span>
-                    </List.Content>
-                  </List.Item>
-                </List>
-              </Segment>
-            </div>
-          );
-        })}
+                          <Label>
+                            <Icon name="cart arrow down"></Icon>marketing
+                          </Label>
+                          <Button
+                            color={btnColor}
+                            as="a"
+                            size="mini"
+                            onClick={(e) => deleteItem(item.key)}
+                          >
+                            <Icon name={iconName} />
+                            {complete}
+                          </Button>
+                        </span>
+                      </List.Content>
+                    </List.Item>
+                  </List>
+                
+                </Segment>
+
+                
+              
+            );
+          })}{" "}
+       
       </div>
     </>
   );
